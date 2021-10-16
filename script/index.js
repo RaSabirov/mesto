1470c7be88f1e67462a489edfc88a50815668a55
Ravil Sabirov <sbrvrvl@ya.ru>
2021-10-16 10:30:26 +0300

    feat: начать делать логику с JS

---
+++ script/index.js
@@ -0,0 +1,27 @@
+// 1. Work with popup
+
+// popup form
+const popupElement = document.querySelector(".popup");
+// popup close
+const popupCloseButtonElement = popupElement.querySelector(".popup__close-btn");
+// popupOpenButton -> edit-btn
+const popupOpenButtonElement = document.querySelector(".profile__edit-btn");
+
+const closePopupByClickOverlay = function(event) {
+  
+}
+
+// Функция отображения popup при клике
+const openPopup = function () {
+  popupElement.classList.add('popup_is-opened');
+};
+
+const closePopup = function (event) {
+  event.preventDefault();
+  popupElement.classList.remove('popup_is-opened');
+};
+
+// Регистрируем обработчики событий при клике
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
+popupElement.addEventListener('click', closePopupByClickOverlay);
