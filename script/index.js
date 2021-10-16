bf9a593486e19e3565bec660a3a5d69fb86f5fd3
Ravil Sabirov <sbrvrvl@ya.ru>
2021-10-16 21:09:52 +0300

    Выполнить небольшие правки по всем блокам

--- script/index.js
+++ script/index.js
@@ -7,20 +7,23 @@ const popupCloseButtonElement = popupElement.querySelector(".popup__close-btn");
 // popupOpenButton -> edit-btn
 const popupOpenButtonElement = document.querySelector(".profile__edit-btn");
 
+// Функция отображения popup при клике на кнопку редактирования
+const openPopup = function () {
+  popupElement.classList.add('popup_is-opened');
+};
+
+// Функция закрытия popup при клике на кнопку закрытия
+const closePopup = function (e) {
+  popupElement.classList.remove('popup_is-opened');
+  e.preventDefault();
+};
+
+// Функция закрытия popup при клике на область вне формы
 const closePopupByClickOverlay = function(event) {
   if (event.target !== event.currentTarget) {
     return;
   }
   closePopup();
-};
-
-// Функция отображения popup при клике
-const openPopup = function () {
-  popupElement.classList.add('popup_is-opened');
-};
-
-const closePopup = function () {
-  popupElement.classList.remove('popup_is-opened');
 };
 
 // Регистрируем обработчики событий при клике
